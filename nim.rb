a429727891183fd80bc9754984dd0274c07c6998
Ivan M. Malo <matheumalo@gmail.com>
2016-11-09 21:34:15 -0500

    Basic nim game without error handling

--- nim.rb
+++ nim.rb
@@ -1,2 +1,16 @@
 
-puts "Welcome to Nim\n Please enter initial number of stones"
+
+puts "Welcome to Nim\nPlease enter initial number of stones\n"
+
+stones = gets.chomp.to_i
+stones_left = 0
+
+while stones > 0
+  puts "#{stones} stones left: " + '*' * stones
+  puts "Remove how many ?"
+  stones_left = gets.chomp.to_i
+  stones -= stones_left
+
+end
+
+puts "Game Over"
